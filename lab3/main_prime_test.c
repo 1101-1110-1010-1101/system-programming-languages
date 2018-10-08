ad076efb804195c1de86f26dd63e3232a40ad0a2
Johnny Kekker <giomail.iv@gmail.com>
2018-10-09 00:27:30 +0300

    lab3 report

--- lab3/main_prime_test.c
+++ lab3/main_prime_test.c
@@ -2,14 +2,17 @@
 #include "is_prime.h"
 
 int main(int argc, char** argv) {
-	unsigned long d;
+	unsigned long d = 0;
 	int res;
 	printf("%s\n", "Enter the number for primality test:");
-	scanf("%lu", &d);
-	res = is_prime(d);
-	if (res == 1)
-		printf("%ld is prime\n", d);
+	if (scanf("%lu", &d)){
+		res = is_prime(d);
+		if (res == 1)
+			printf("%lu is prime\n", d);
+		else
+			printf("%lu is not prime\n", d);
+	}
 	else
-		printf("%ld is not prime\n", d);
+		printf("%s\n", "Value should be a number");
 	return 0;
 }
