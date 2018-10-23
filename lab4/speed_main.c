d611d72d7ce937531e38b99e9283e76db1bceb8a
Johnny Kekker <giomail.iv@gmail.com>
2018-10-23 11:27:25 +0300

    reformat

--- lab4/speed_main.c
+++ lab4/speed_main.c
@@ -22,90 +22,77 @@ int main(int argc, char *argv[]) {
     clock_t add_back_1000_begin = clock();
     list_add_back(20, &list1000);
     clock_t add_back_1000_end = clock();
-    double add_back_1000_time = (double) (add_back_1000_end - add_back_1000_begin);
-    printf("list_add_back_1000: %.15fns\n", add_back_1000_time);
+    printf("list_add_back_1000: %lu us\n", (add_back_1000_end - add_back_1000_begin));
 
     clock_t add_middle_1000_begin = clock();
     list_add_by_index(20, &list1000, 500);
     clock_t add_middle_1000_end = clock();
-    double add_middle_1000_time = (double) (add_middle_1000_end - add_middle_1000_begin);
-    printf("list_add_middle_1000: %.15fns\n", add_middle_1000_time);
+    printf("list_add_middle_1000: %lu us\n", (add_middle_1000_end - add_middle_1000_begin)e);
 
     clock_t add_front_1000_begin = clock();
     list_add_front(20, &list1000);
     clock_t add_front_1000_end = clock();
-    double add_front_1000_time = (double) (add_front_1000_end - add_front_1000_begin);
-    printf("list_add_front_1000: %.15fns\n", add_front_1000_time);
+    printf("list_add_front_1000: %lu us\n", (add_front_1000_end - add_front_1000_begin));
 
     clock_t delete_back_1000_begin = clock();
     delete_back(&list1000);
     clock_t delete_back_1000_end = clock();
-    double delete_back_1000_time = (double) (delete_back_1000_end - delete_back_1000_begin);
-    printf("list_delete_back_1000: %.15fns\n", delete_back_1000_time);
+    printf("list_delete_back_1000: %lu us\n", (delete_back_1000_end - delete_back_1000_begin));
 
     clock_t delete_front_1000_begin = clock();
     delete_front(&list1000);
     clock_t delete_front_1000_end = clock();
-    double delete_front_1000_time = (double) (delete_front_1000_end - delete_front_1000_begin);
-    printf("list_delete_front_1000: %.15fns\n", delete_front_1000_time);
+    printf("list_delete_front_1000: %lu us\n", (delete_front_1000_end - delete_front_1000_begin));
 
     clock_t add_back_50_begin = clock();
     list_add_back(20, &list50);
     clock_t add_back_50_end = clock();
-    double add_back_50_time = (double) (add_back_50_end - add_back_50_begin);
-    printf("list_add_back_50: %.15fns\n", add_back_50_time);
+    double add_back_50_time = (double) ;
+    printf("list_add_back_50: %lu us\n", (add_back_50_end - add_back_50_begin));
 
     clock_t add_middle_50_begin = clock();
     list_add_by_index(20, &list50, 25);
     clock_t add_middle_50_end = clock();
     double add_middle_50_time = (double) (add_middle_50_end - add_middle_50_begin);
-    printf("list_add_middle_50: %.15fns\n", add_middle_50_time);
+    printf("list_add_middle_50: %lu us\n", (add_middle_50_end - add_middle_50_begin));
 
     clock_t add_front_50_begin = clock();
     list_add_front(20, &list50);
     clock_t add_front_50_end = clock();
-    double add_front_50_time = (double) (add_front_50_end - add_front_50_begin);
-    printf("list_add_front_50: %.15fns\n", add_front_50_time);
+    printf("list_add_front_50: %lu us\n", (add_front_50_end - add_front_50_begin));
 
     clock_t delete_back_50_begin = clock();
     delete_back(&list50);
     clock_t delete_back_50_end = clock();
-    double delete_back_50_time = (double) (delete_back_50_end - delete_back_50_begin);
-    printf("list_delete_back_50: %.15fns\n", delete_back_50_time);
+    printf("list_delete_back_50: %lu us\n", (delete_back_50_end - delete_back_50_begin));
 
     clock_t delete_front_50_begin = clock();
     delete_front(&list50);
     clock_t delete_front_50_end = clock();
-    double delete_front_50_time = (double) (delete_front_50_end - delete_front_50_begin);
-    printf("list_delete_front_50: %.15fns\n", delete_front_50_time);
+    printf("list_delete_front_50: %lu us\n", (delete_front_50_end - delete_front_50_begin));
 
     clock_t add_back_5_begin = clock();
     list_add_back(20, &list5);
     clock_t add_back_5_end = clock();
-    double add_back_5_time = (double) (add_back_5_end - add_back_5_begin);
-    printf("list_add_back_5: %.15fns\n", add_back_5_time);
+    printf("list_add_back_5: %lu us\n", (add_back_5_end - add_back_5_begin));
 
     clock_t add_middle_5_begin = clock();
     list_add_by_index(20, &list5, 2);
     clock_t add_middle_5_end = clock();
-    double add_middle_5_time = (double) (add_middle_5_end - add_middle_5_begin);
-    printf("list_add_middle_5: %.15fns\n", add_middle_5_time);
+    printf("list_add_middle_5: %lu us\n", (add_middle_5_end - add_middle_5_begin));
 
     clock_t add_front_5_begin = clock();
     list_add_front(20, &list5);
     clock_t add_front_5_end = clock();
-    double add_front_5_time = (double) (add_front_5_end - add_front_5_begin);
-    printf("list_add_front_5: %.15fns\n", add_front_5_time);
+    printf("list_add_front_5: %lu us\n", (add_front_5_end - add_front_5_begin));
 
     clock_t delete_back_5_begin = clock();
     delete_back(&list5);
     clock_t delete_back_5_end = clock();
-    double delete_back_5_time = (double) (delete_back_5_end - delete_back_5_begin);
-    printf("list_delete_back_5: %.15fns\n", delete_back_5_time);
+    printf("list_delete_back_5: %lu us\n", (delete_back_5_end - delete_back_5_begin));
 
     clock_t delete_front_5_begin = clock();
     delete_front(&list5);
     clock_t delete_front_5_end = clock();
-    double delete_front_5_time = (double) (delete_front_5_end - delete_front_5_begin);
-    printf("list_delete_front_5: %.15fns\n", delete_front_5_time);
+    printf("list_delete_front_5: %lu us\n", (delete_front_5_end - delete_front_5_begin));
 }
