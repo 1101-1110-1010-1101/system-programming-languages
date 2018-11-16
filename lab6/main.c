638f6ab82bcd2b5515e453424ab41c914af13456
johnny-keker <giomail.iv@gmail.com>
2018-11-16 19:46:08 +0300

    min query and simple init

--- lab6/main.c
+++ lab6/main.c
@@ -5,7 +5,7 @@
 #include <sys/mman.h>
 #include <stdbool.h>
 
-#define MIN_BLOCK_SIZE (sizeof(block_header) + 8)
+#define MIN_QUERY_SIZE 8
 #define INIT_BLOCK_SIZE 4096
 
 typedef struct block_header block_header;
@@ -17,13 +17,29 @@ struct __attribute__((packed)) block_header {
   bool is_free;
 };
 
-void* customalloc(size_t query) {
+block_header* init() {
+  start = mmap(NULL, INIT_BLOCK_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
   block_header* header = (block_header*)start;
-  while (header->is_free == false || header->capacity < query)
+  header->next = NULL;
+  header->capacity = INIT_BLOCK_SIZE;
+  header->is_free = true;
+  return header;
+}
+
+void* customalloc(size_t query) {
+  if (query < MIN_QUERY_SIZE)
+    query = MIN_QUERY_SIZE;
+  block_header* header = (block_header*)start;
+  while (header->is_free == false || header->capacity < query) {
     header = header->next;
+    if (header == NULL) {
+      header = init();
+      break;
+    }
+  }
   header->is_free = false;
   // so, next step - we need to transfer remaining space to another block
-  if (header->capacity - query >= MIN_BLOCK_SIZE) {
+  if (header->capacity - query >= MIN_QUERY_SIZE + sizeof(block_header)) {
     block_header* new_header = (block_header*)((uint8_t*)header + query + sizeof(block_header));
     new_header->capacity = header->capacity - query - sizeof(block_header);
     new_header->is_free = true;
@@ -34,13 +50,8 @@ void* customalloc(size_t query) {
 }
 
 int main() {
-  start = mmap(NULL, INIT_BLOCK_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
-  block_header* header = (block_header*)start;
-  header->next = NULL;
-  header->capacity = INIT_BLOCK_SIZE;
-  printf("sizeof header = %ld\n", sizeof(block_header));
-  header->is_free = true;
-  void* smt = customalloc(96);
-  void* smt_else = customalloc(96);
+  block_header* header = init();
+  void* smth = customalloc(4000);
+  void* smth_else = customalloc(1000);
   return 0;
 }
