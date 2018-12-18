8da7ec2c9503c40771e3e5bf3b6ebc440ad7609d
johnny-keker <giomail.iv@gmail.com>
2018-12-18 03:05:34 +0300

    [lab7] avx implementation, @timlathy - you`re my personal hero!

--- lab7/main.c
+++ lab7/main.c
@@ -9,11 +9,21 @@
 #include <unistd.h>
 
 #define exit_with_err(...) do { fprintf(stderr, __VA_ARGS__); return 1; } while (0);
-void image_sepia_sse(pixel_t* pixel, uint32_t size);
- 
+extern void image_sepia_sse(pixel_t* pixel, uint64_t size);
+extern void image_sepia_avx(pixel_t* pixel, uint64_t size);
+
 #define BENCHMARK_NUM_RUNS 250UL
 
 
+void sepia_avx_inplace(image_t* img) {
+  uint64_t num_pixels = img->height * img->width;
+
+  if (num_pixels < 8) { for (int p = 0; p < num_pixels; ++p) sepia_one(img->data + p); return; }
+
+  image_sepia_avx(img->data, num_pixels - num_pixels % 8);
+
+  for (int p = num_pixels - num_pixels % 8; p < num_pixels; ++p) sepia_one(img->data + p);
+}
 
 
 void sepia_sse_inplace(image_t *img) {
@@ -33,6 +43,17 @@ void sepia_sse_inplace(image_t *img) {
 void calculate_time(image_t* img) {
   struct rusage r;
   struct timeval start, end;
+  
+  getrusage(RUSAGE_SELF, &r); start = r.ru_utime;
+
+  for (uint64_t i = 0; i < BENCHMARK_NUM_RUNS; i++) sepia_avx_inplace(img);
+
+  getrusage(RUSAGE_SELF, &r); end = r.ru_utime;
+
+  long res = ((end.tv_sec - start.tv_sec) * 1000000L) + end.tv_usec - start.tv_usec;
+
+  printf("AVX: %ld μs (average for %lu runs)\n", res / BENCHMARK_NUM_RUNS, BENCHMARK_NUM_RUNS);
+
 
   getrusage(RUSAGE_SELF, &r); start = r.ru_utime;
 
@@ -40,7 +61,7 @@ void calculate_time(image_t* img) {
 
   getrusage(RUSAGE_SELF, &r); end = r.ru_utime;
 
-  long res = ((end.tv_sec - start.tv_sec) * 1000000L) + end.tv_usec - start.tv_usec;
+  res = ((end.tv_sec - start.tv_sec) * 1000000L) + end.tv_usec - start.tv_usec;
 
   printf("SSE: %ld μs (average for %lu runs)\n", res / BENCHMARK_NUM_RUNS, BENCHMARK_NUM_RUNS);
 
@@ -68,6 +89,7 @@ int main(int argc, char** argv) {
     exit_with_err("Cannot open %s\n", input_name);
   FILE* output_c = fopen("out/result_c.bmp", "wb");
   FILE* output_sse = fopen("out/result_sse.bmp", "wb");
+  FILE* output_avx = fopen("out/result_avx.bmp", "wb");
   image_t* img = (image_t*) malloc(sizeof(image_t));
   read_status r_st = read_bmp_data(input, img);
   image_t* img_calc = (image_t*)malloc(sizeof(image_t));
@@ -93,9 +115,15 @@ int main(int argc, char** argv) {
   sepia_sse_inplace(img);
   write_bmp(img, output_sse);
 
+  image_t* img_avx = (image_t*)malloc(sizeof(image_t));
+  r_st = read_bmp_data(input, img_avx);
+  sepia_avx_inplace(img_avx);
+  write_bmp(img_avx, output_avx);
+
   fclose(input);
   fclose(output_c);
   fclose(output_sse);
+  fclose(output_avx);
 
   return 0;
 }
